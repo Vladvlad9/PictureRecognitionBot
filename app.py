51c3466b38b1940f1af20595301dd2f850f436b2
Vlad Paulechka <vlad9.ru@gmail.com>
2023-02-01 12:21:38 +0300

    работа с фото

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-import asyncio
-from datetime import datetime
-
-from aiogram.utils.exceptions import ChatNotFound
-
-from loader import bot
 from utils.set_bot_commands import set_default_commands
 
 
@@ -12,7 +6,7 @@ async def on_startup(_):
 
 
 if __name__ == '__main__':
-    from aiogram import executor
+    from aiogram import executor, types
     from handlers import dp
 
     executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,16 +1,13 @@
-from PIL import Image
 from aiogram import types
-from aiogram.dispatcher.storage import FSMContext
-from aiogram.utils.exceptions import BadRequest
+
 from loader import dp, bot
 
 import easyocr
 
 
 async def text_description(file_path):
-    reader = easyocr.Reader(["ru", "en"])
+    reader = easyocr.Reader(["ru", "en", "be"])
     result = reader.readtext(file_path, detail=0, paragraph=True)
-
     return result
 
 
@@ -18,7 +15,7 @@ async def text_description(file_path):
 async def process_message(message: types.Message):
     photo = await bot.get_file(message.photo[len(message.photo) - 1].file_id)
     await bot.download_file(photo.file_path, destination='new_img.jpg', timeout=12, chunk_size=1215000)
-
+    await message.answer(text="Ожидайте, я обрабатываю картинку")
     get_photo = await text_description(file_path="new_img.jpg")
     if get_photo:
         await message.answer(text=get_photo)
@@ -29,4 +26,5 @@ async def process_message(message: types.Message):
 
 @dp.message_handler(commands=["start"])
 async def registration_start(message: types.Message):
-    await message.answer(text="Привет!")
+    await message.answer(text="Добро пожаловать в бот по распознаванию изображений\n\n"
+                              "Что бы воспользоваться моим функционалом просто отправь мне картинку")
